81af0648508922a853f7c2ec5682b1052dfa55ad
ems236 <ems236@case.edu>
2019-10-01 14:47:49 -0400

    more osrf setup

--- src/ariac_runner.cpp
+++ src/ariac_runner.cpp
@@ -1,5 +1,9 @@
 #include "ros/ros.h"
 #include "std_srvs/Trigger.h"
+#include "osrf_gear/Order.h"
+#include "osrf_gear/Kit.h"
+#include "osrf_gear/StorageUnit.h"
+#include "osrf_gear/GetMaterialLocations"
 #include <cstdlib>
 #include <string>
 #include <vector>
@@ -7,19 +11,24 @@
 using namespace std;
 
 bool has_started_competition = false;
+vector<osrf_gear::Order> current_orders;
+int current_kit_index = 0;
 
-void try_start_competition(ros::ServiceClient begin_client)
+bool try_start_competition(ros::ServiceClient& begin_client)
 {
+
     std_srvs::Trigger begin_comp;
     if(begin_client.call(begin_comp))
     {
         if(begin_comp.response.success)
         {
             has_started_competition = true;
+            return true;
         }
         else
         {
             ROS_WARN("Start competition unsuccessful. Message %s", begin_comp.response.message.c_str());
+            return false;
         }
     }
     else
@@ -27,6 +36,13 @@ void try_start_competition(ros::ServiceClient begin_client)
         ROS_ERROR("Error contacting competition service.");
         exit(0);
     }
+}
+
+
+
+void new_order_callback(const osrf_gear::Order::ConstPtr& new_order)
+{
+    current_orders.push_back(*new_order);
 }
 
 int main(int argc, char** argv)
@@ -38,18 +54,26 @@ int main(int argc, char** argv)
     ROS_INFO("Detected namespace %s", ns.c_str());
 
     ros::ServiceClient begin_client = node_handle.serviceClient<std_srvs::Trigger>("/ariac/start_competition");
-
-    //ros::Subscriber velocity_subscriber = node_handle.subscribe(read_velocity_topic, 200, desired_velocity_callback);
+    ros::Subscriber order_subscriber = node_handle.subscribe("/ariac/orders", 200, new_order_callback);
     
-    ros::Rate loop_rate(10);
+    //Spin slow until competition starts
+    ros::Rate loop_rate(0.2);
 
     //Main loop
     while(ros::ok())
     {
         if(!has_started_competition)
         {
-
+            if(try_start_competition(begin_client))
+            {
+                loop_rate = ros::Rate(10);
+            }
         }
+        else
+        {
+            
+        }
+        
 
         //process all callbacks
         ros::spinOnce();
