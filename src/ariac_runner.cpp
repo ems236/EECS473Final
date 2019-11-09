44da6af69e6a15d0ffa866f6a7b76d61bc9abd2a
ems236 <ems236@case.edu>
2019-11-09 14:00:03 -0500

    renamed

--- src/ariac_runner.cpp
+++ src/ariac_runner.cpp
@@ -228,9 +228,9 @@ geometry_msgs::PoseStamped logical_camera_to_base_link(tf2_ros::Buffer& tfBuffer
     geometry_msgs::PoseStamped local_pose, world_pose, base_link_pose;
     local_pose.pose = logical_pose;
 
-    tf2::doTransform(local_pose, world_pose, tfStamped);
+    tf2::doTransform(local_pose, world_pose, tf_logical_to_world);
     offset_target_position(world_pose);
-    tf2::doTransform(world_pose, base_link_pose);
+    tf2::doTransform(world_pose, base_link_pose, tf_world_to_base_link);
 
     return base_link_pose;
 }
