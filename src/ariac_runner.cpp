3812018f7135fbe0829f2ca8a676aac0257ec5da
ems236 <ems236@case.edu>
2019-11-21 13:17:17 -0500

    added more info

--- src/ariac_runner.cpp
+++ src/ariac_runner.cpp
@@ -442,6 +442,8 @@ int main(int argc, char** argv)
     initialize_trajectory(joint_trajectory_as);
     add_home_point_to_trajectory(joint_trajectory_as, ros::Duration(1.5));
     actionlib::SimpleClientGoalState state = trajectory_as.sendGoalAndWait(joint_trajectory_as.action_goal.goal, ros::Duration(10.0), ros::Duration(3.0));
+    ROS_INFO("Action Server returned with status: [%i] %s", state.state_, state.toString().c_str());
+
     ros::Duration(1.0).sleep();
     ROS_INFO("Moved home");
 
